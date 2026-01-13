fbbb8b109bf959bd98f596f17540e09b28a10c51
MthoNtanzi <mthontanzi99@gmail.com>
2026-01-13 13:54:37 +0200

    added post logic

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 const addTaskBtn = document.getElementById('addTaskBtn');
 
 // Array
-const taskJson = {};
+const taskJson = [];
 
+// Create a form that will take in user input, used to input tasks
 const taskForm = document.createElement('form');
 taskForm.id = 'taskForm';
 
@@ -18,9 +19,49 @@ taskForm.innerHTML = `
             <option class="dropdown" value="high">High</option>
             <option class="dropdown" value="urgent">Urgent</option>
         </select>
-        <button id="createTaskBtn">Create Task</button>
+        <button id="createTaskBtn" type="submit">Create Task</button>
     </div>
 `;
+// Append task form to the body
+const mainElement = document.querySelector('main');
+mainElement.appendChild(taskForm);
 
-const bodyElement = document.querySelector('body');
-bodyElement.appendChild(taskForm);
+// show task form
+addTaskBtn.addEventListener('click',()=>{
+    taskForm.style.display = 'block';
+});
+
+// hide task form when user clicks outside
+taskForm.addEventListener('click', (e) => {
+  if (e.target === taskForm) {
+    taskForm.style.display = 'none';
+  }
+});
+
+let taskCount = 1;
+const task = taskJson[taskJson.length - 1];
+// Add Task
+taskForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    const taskName = document.getElementById('taskName').value.trim();
+    const priority = document.getElementById('taskPriority').value;
+
+    if(!taskName) return;
+
+    taskJson.push({
+        id: "task"+taskCount,
+        taskName,
+        priority,
+        completed: false
+    });
+
+    // let todoActivity = `
+    //     <li><img src="images/checkbox-checked.png">${taskJson}</li>
+    // `;
+
+    console.log(taskJson);
+    taskForm.reset();
+
+    taskCount++;
+});
